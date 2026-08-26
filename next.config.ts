38af264b8848cd17e2b10a96abb91065d310d853
utsablpi <utsabkundu.laserpowerinfra@gmail.com>
2026-08-26 18:32:46 +0530

    added github action

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname),
   serverExternalPackages: ["@prisma/client", "@prisma/adapter-pg", "pg"],
   allowedDevOrigins: ["192.168.1.200", "192.168.1.229", "192.168.1.190" ,"172.24.112.1","192.168.1.196"],
+  output: "standalone",
 };
 
 export default nextConfig;
